74436d23933cbc7e5f281539f43240c94b1154e5
EshginCeferli <107032407+EshginCeferli@users.noreply.github.com>
2022-08-18 00:41:57 +0400

    Add files via upload

---
+++ Js-intro/assets/js/script.js
@@ -0,0 +1,94 @@
+// alert("Hello")
+// console.log("Salamlar")
+
+//var, let, const
+
+// var name = "Esqin";
+// var surname = "Ceferli"
+
+// let age = 10;
+// let age = 20;
+
+// let age;
+// age = 10;
+
+
+// console.log(name + " " + surname);
+
+// console.log(`${name} ${surname}`)
+
+// const name = "Esqin";
+// name = "Haqqi";
+
+// console.log(name)
+
+// let age = 30;
+// let name = "Seid"
+// let surname = "Nureliyev"
+// let address = null;
+// let count;
+// console.log(address)
+// console.log(count);
+
+// let person = {
+//     name: "Orxan",
+//     surname: "Ekberov",
+//     age: 20,
+//     phoneNumbers: [13131212, 1313213]
+// }
+
+// let person2 = {
+//     name: "Nermin",
+//     surname: "Memmedova",
+//     age: 28,
+//     phoneNumbers: [13131212, 123131231]
+// }
+
+// console.log(person);
+// console.log(person.surname)
+
+// let students = ["Ramil", "Emil", 40];
+// let students = [person, person2]
+
+// for (let i = 0; i < students.length; i++) {
+//     for (let j = 0; j < students[i].phoneNumbers.length; j++) {
+//         console.log(students[i].phoneNumbers[j])
+//     }
+// }
+
+// let numbers = [2, 3, 4, 5, 6, 7, 8];
+// numbers.forEach(item => {
+//     console.log(item)
+// });
+
+// let num1 = 66;
+// let num2 = 30;
+
+// let result = 66 % 30;
+// console.log(result)
+
+
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 == 0) {
+//         console.log("Tek" + i)
+//     }
+//     else {
+//         console.log("Cut")
+//     }
+// }
+
+// let studentSurname = "Binnetov";
+// let result = studentSurname ?? "Not found"
+
+
+// let studenSurname = "ddw";
+// if (studenSurname == undefined) {
+//     console.log("Teyin olunmayib")
+// }
+// else {
+//     console.log("Teyin olunub")
+// }
+
+// let isDeleted =null;
+
+// console.log(typeof isDeleted)
